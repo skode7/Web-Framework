da8f14407c9aae1667658ecd1b431b03599004d6
Teemu <poutanenteemu1@gmail.com>
2026-03-17 11:55:04 +0200

    show results in html

--- Week_1/JS_recap_1/Task_1/main.js
+++ Week_1/JS_recap_1/Task_1/main.js
@@ -3,9 +3,11 @@ const tempInCelsius = parseFloat(prompt('Enter temperature in celsius: '));
 function convert(temp) {
   const tempInFahrenheit = (tempInCelsius * 9) / 5 + 32;
   const tempInKelvin = tempInCelsius + 273.15;
+  const resultString = `
+    ${tempInCelsius} celsius in fahrenheit is: ${tempInFahrenheit}
+    ${tempInCelsius} celsius in kelvin is: ${tempInKelvin}`;
 
-  console.log('Temp in fahrenheit: ' + tempInFahrenheit);
-  console.log('Temp in kelvin: ' + tempInKelvin);
+  document.querySelector('p').innerText = resultString;
 }
 
 convert(tempInCelsius);
