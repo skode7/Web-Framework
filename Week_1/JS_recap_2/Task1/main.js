d25024234c95fce62147dfbe0c4046feac0a9b74
Teemu <poutanenteemu1@gmail.com>
2026-03-19 11:42:48 +0200

    task1 done

---
+++ Week_1/JS_recap_2/Task1/main.js
@@ -0,0 +1,21 @@
+fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+console.log('Fruits: ' + fruits);
+console.log('Length of fruits: ' + fruits.length);
+
+console.log('Element at index 2: ' + fruits[2]);
+
+console.log('Last element of fruits: ' + fruits[fruits.length - 1]);
+
+vegetables = [];
+
+veg1 = prompt('Enter first vegetable: ');
+veg2 = prompt('Enter second vegetable: ');
+veg3 = prompt('Enter third vegetable: ');
+
+vegetables.push(veg1);
+vegetables.push(veg2);
+vegetables.push(veg3);
+
+console.log('Vegetables: ' + vegetables);
+console.log('Length of Vegetables: ' + vegetables.length);
