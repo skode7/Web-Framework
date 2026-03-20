d97e46ba097a5e306d0038ac80f551fd78ca982a
Teemu <poutanenteemu1@gmail.com>
2026-03-20 17:02:08 +0200

    task4 done

---
+++ Week_1/JS_recap_2/Task_4/main.js
@@ -0,0 +1,7 @@
+const array = [-1, 1, 5, 3, 2, 0];
+console.log('Original array: ' + array);
+
+function sortArray(array) {
+  return array.toSorted();
+}
+console.log('Sorted array: ', sortArray(array));
