549ce828fc5d50363fc1990fe9e3aa64c724c636
Teemu <poutanenteemu1@gmail.com>
2026-03-23 14:55:55 +0200

    t3 done

--- Week_2/JS_recap_3/dom-assignments/t3/t3.js
+++ Week_2/JS_recap_3/dom-assignments/t3/t3.js
@@ -1,4 +1,16 @@
-const div = document.querySelector('#target');
+const myDiv = document.querySelector('#target');
 
-const browserName = navigator.appCodeName;
-const operatingSystem = navigator.console.log(navigator);
+const nameAndVersionAndOs = navigator.userAgent;
+const width = window.screen.width;
+const height = window.screen.height;
+const screenSpace = window.innerWidth + 'x' + window.innerHeight;
+const currentDate = new Date().toLocaleDateString('fi-FI');
+const currentTime = new Date().toLocaleTimeString('fi-FI');
+
+myDiv.innerHTML = `
+<p>Name, version and OS: ${nameAndVersionAndOs}</p>
+<p>width: ${width}</p>
+<p>height: ${height}</p>
+<p>screen space: ${screenSpace}</p>
+<p>current date: ${currentDate}</p>
+<p>current time: ${currentTime}</p>`;
