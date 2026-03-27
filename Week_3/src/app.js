f01b21f65ce501251ec3a60ba51e7b60e6ee76c4
Teemu <poutanenteemu1@gmail.com>
2026-03-27 12:47:58 +0200

    add endpoint to serve response to get request

--- Week_3/src/app.js
+++ Week_3/src/app.js
@@ -4,8 +4,17 @@ const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my REST API!');
+app.get('/api/v1/cat', (req, res) => {
+  res.send(
+    JSON.stringify({
+      cat_id: 5,
+      name: 'Kissa',
+      birthdate: 'Yesterday',
+      weight: '0.4kg',
+      owner: 'Teemu',
+      image: 'https://loremflickr.com/320/240/cat',
+    })
+  );
 });
 
 app.listen(port, hostname, () => {
