43e76c771ccbbee06136d93937cc9c3751d27686
Teemu <poutanenteemu1@gmail.com>
2026-03-28 20:19:56 +0200

    cat routes and user routes done

--- Week_3/03_routing/src/index.js
+++ Week_3/03_routing/src/index.js
@@ -1,22 +1,13 @@
 import app from './app.js';
-/* import catRouter from './api/routes/catRouter.js';
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-app.use('/api/v1/cat', catRouter);
-
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
- */
-
 import userRouter from './api/routes/userRouter.js';
+import catRouter from './api/routes/catRouter.js';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 app.use('/api/v1/user', userRouter);
+
+app.use('/api/v1/cat', catRouter);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
