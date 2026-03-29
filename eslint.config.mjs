7f046e18a06bf1604b5f966da0dcda73a4111107
Teemu <poutanenteemu1@gmail.com>
2026-03-29 20:29:28 +0300

    add ...globals.browser, in languageoptions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,11 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs}'],
     plugins: {js},
     extends: ['js/recommended'],
-    languageOptions: {globals: globals.node},
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
   },
 ]);
