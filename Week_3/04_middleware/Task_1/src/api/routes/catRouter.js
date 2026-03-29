bf576e7c785596127dab6fd9b7d00166ac47ecc7
Teemu <poutanenteemu1@gmail.com>
2026-03-29 16:28:45 +0300

    add: file upload

--- Week_3/04_middleware/Task_1/src/api/routes/catRouter.js
+++ Week_3/04_middleware/Task_1/src/api/routes/catRouter.js
@@ -1,3 +1,4 @@
+import multer from 'multer';
 import express from 'express';
 import {
   getCats,
@@ -8,8 +9,10 @@ import {
 } from '../controllers/catController.js';
 
 const catRouter = express.Router();
+const upload = multer({dest: './Week_3/04_middleware/public/uploads/'});
 
-catRouter.route('/').get(getCats).post(addCat);
+catRouter.route('/').get(getCats);
+catRouter.route('/').post(upload.single('image'), addCat);
 
 catRouter.route('/:id').get(getCatByID).put(putCat).delete(deleteCat);
 
