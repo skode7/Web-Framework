8b11a87fab4839a0d0b661ceaf81283353bf7223
Teemu <poutanenteemu1@gmail.com>
2026-04-01 13:21:43 +0300

    add endpoint for auth

--- Week_3/06_auth/src/index.js
+++ Week_3/06_auth/src/index.js
@@ -1,6 +1,7 @@
 import app from './app.js';
 import userRouter from './api/routes/userRouter.js';
 import catRouter from './api/routes/catRouter.js';
+import authRouter from './api/routes/authRouter.js';
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -8,6 +9,8 @@ const port = 3000;
 app.use('/api/v1/user', userRouter);
 
 app.use('/api/v1/cat', catRouter);
+
+app.use('/api/v1auth/login', authRouter);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
