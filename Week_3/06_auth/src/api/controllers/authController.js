cd16ffb56f3d38070e52d7db9e32dbee8c2cae56
Teemu <poutanenteemu1@gmail.com>
2026-04-02 17:45:40 +0300

    remove console.logs

--- Week_3/06_auth/src/api/controllers/authController.js
+++ Week_3/06_auth/src/api/controllers/authController.js
@@ -32,8 +32,6 @@ const postLogin = async (req, res) => {
 };
 
 const getMe = async (req, res) => {
-  console.log('getMe', res.locals.user);
-
   if (res.locals.user) {
     res.json({message: 'token ok', user: res.locals.user});
   } else {
