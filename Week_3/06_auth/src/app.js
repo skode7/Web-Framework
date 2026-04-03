616a9b2e3042d71837625ac3c99275f5f6e2e4bd
Teemu <poutanenteemu1@gmail.com>
2026-04-03 14:44:29 +0300

    change express.static public folder path  to absolute path

--- Week_3/06_auth/src/app.js
+++ Week_3/06_auth/src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
-app.use('/public', express.static('/Week_3/06_auth/src/public'));
+app.use(
+  '/public',
+  express.static(path.join(process.cwd(), '/Week_3/06_auth/src/public'))
+);
 
 export default app;
