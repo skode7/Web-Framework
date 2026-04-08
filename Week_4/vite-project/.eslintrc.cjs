697ce94e7c2ea4f20688080064f3f79b32d517e5
Teemu <poutanenteemu1@gmail.com>
2026-04-08 14:39:31 +0300

    first component

---
+++ Week_4/vite-project/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  // Huom: Vain yksi plugins-lista
+  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {version: '18.2'},
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  // Huom: Vain yksi rules-objekti
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      {allowConstantExport: true},
+    ],
+    'react/prop-types': 'off',
+  },
+};
---
+++ Week_4/vite-project/src/components/MediaRow.jsx
@@ -0,0 +1,17 @@
+const MediaRow = (props) => {
+  const {item} = props;
+  return (
+    <tr key={item.media_id}>
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{new Date(item.created_at).toLocaleDateString('fi-FI')}</td>
+      <td>{item.filesize}</td>
+      <td>{item.media_type}</td>
+    </tr>
+  );
+};
+
+export default MediaRow;
