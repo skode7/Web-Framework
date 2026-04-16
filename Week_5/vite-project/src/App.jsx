f0622d434b33683a8e83ea413951f6efa63ebee2
Teemu <poutanenteemu1@gmail.com>
2026-04-16 15:41:49 +0300

    add protected route

--- Week_5/vite-project/src/App.jsx
+++ Week_5/vite-project/src/App.jsx
@@ -5,20 +5,33 @@ import Upload from './views/Upload.jsx';
 import Single from './views/Single.jsx';
 import Login from './views/Login.jsx';
 import Layout from './components/Layout.jsx';
+import {UserProvider} from './contexts/UserContext';
+import Logout from './views/Logout.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 const App = () => {
   return (
     <>
       <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/upload" element={<Upload />} />
-            <Route path="/single" element={<Single />} />
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
+        <UserProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/upload" element={<Upload />} />
+              <Route path="/single" element={<Single />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
+            </Route>
+          </Routes>
+        </UserProvider>
       </BrowserRouter>
     </>
   );
